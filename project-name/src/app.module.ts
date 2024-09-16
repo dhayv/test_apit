fb6cd900cbd7c3d14689eb2f1ea89e9dab6f40a4
Hayvid <davidhyppolitedh@gmail.com>
2024-09-16 14:47:23 -0400

    added user controller

--- project-name/src/app.module.ts
+++ project-name/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController],
+  controllers: [AppController, CatsController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
